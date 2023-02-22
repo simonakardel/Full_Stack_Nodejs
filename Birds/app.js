33e4ee768fb67e7fd866633bec6f28529e99a2f8
simonatingleffkardel <simonakardel@gmail.com>
2023-02-22 15:40:43 +0100

    added full crud to birds API

--- Birds/app.js
+++ Birds/app.js
@@ -1,17 +1,7 @@
-const app = require('express')();
+const express = require("express");
+const app = express();
 
-// const birds = new Map();
-// birds.set(1, { name: "Bald Eagle", habitat: "North America" });
-// birds.set(2, { name: "Blue Jay", habitat: "North America" });
-// birds.set(3, { name: "Penguin", habitat: "Antarctica" });
-// birds.set(4, { name: "Toucan", habitat: "Central and South America" });
-// birds.set(5, { name: "Kookaburra", habitat: "Australia" });
-// birds.set(6, { name: "Flamingo", habitat: "South America" });
-// birds.set(7, { name: "Hawk", habitat: "North America" });
-// birds.set(8, { name: "Pelican", habitat: "Coastal regions" });
-// birds.set(9, { name: "Ostrich", habitat: "Africa" });
-// birds.set(10, { name: "Pigeon", habitat: "Cities" });
-
+app.use(express.json());
 
 const birdsArray = [
     { id: 1, name: "Bald Eagle", habitat: "North America" },
@@ -32,13 +22,60 @@ function getBirdById(id){
 
 // ALL BIRDS
 app.get('/birds', (req, res) => {
-    res.send(JSON.stringify(birdsArray));
-})
+    res.send({data : birdsArray});
+});
 
 // BIRDS BY ID
 app.get('/birds/:id', (req, res) => {
-    res.send(JSON.stringify(getBirdById(parseInt(req.params.id))));
-})
+    res.send({data: getBirdById(parseInt(req.params.id))});
+});
+
+// CREATE BIRD
+// CHECKS IF BIRD WITH GIVEN ID ALREADY EXISTS
+app.post('/birds', (req, res) => {
+    const newBird = req.body;
+    const bird = getBirdById(newBird.id)
+    if(bird){
+        res.send({message: `Bird with id ${newBird.id} already exists, choose another id`})
+    } else { 
+        birdsArray.push(newBird);
+        res.send({ message: newBird });
+    }
+    console.log(birdsArray)
+});
 
 
-app.listen(8080);
+// DELETE BIRD BY ID
+// CHECKS IF BIRD WITH GIVEN ID EXISTS FIRST
+app.delete('/birds/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const bird = getBirdById(id);
+    if(!bird){
+        res.send({message:`Bird with id ${id} not found` })
+    } else {
+        res.send({message: `Bird deleted with id ${id}`})
+        const index = birdsArray.findIndex(bird => bird.id === id)
+        birdsArray.splice(index, 1)
+    }
+    console.log(birdsArray)
+});
+
+// UPDATE BIRD BY ID
+// CHECKS IF BIRD WITH GIVEN ID EXISTS FIRST
+app.put('/birds/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const bird = getBirdById(id);
+    const updatedBird = req.body;
+    if(!bird){
+        res.send({message:`Bird with id ${id} not found` })
+    } else {
+        const index = birdsArray.findIndex(bird => bird.id === id)
+        birdsArray[index] = updatedBird;
+        res.send({message: `Bird with id ${id} was updated`})
+    }
+    console.log(birdsArray)
+});
+
+app.listen(8080, () => {
+    console.log("Server is running on port", 8080);
+});
